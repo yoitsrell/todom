4c8edc4e6eec1c0f5cbb39e4b307a8364377cde6
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-03-23 05:26:11 -0400

    Hard time figuring out remove

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ An exercise in putting todo lists on the DOM.
 ### Basic Steps
 
 * First, let's rewrite our `printTodo` function so that it takes in a string and, instead of console.logging it, adds it as `innerText` to an `li` that we append to our `ul`.
-* 
+  
 * Now our print-everything function should, since it's using `printTodo` internally, put everything from our todo list on the DOM!
-* 
+  
 * Now let's write a function that can clear the `ul` of `li`s.
-* 
+  
 * Now one more function that ties that all together - a function that calls the dom-clearing-function followed by the print-all function. Now we can always call that function to refresh our DOM, removing anything in our user's todo list and repopulating it based on todos.
 
 * Okay, almost done. Now we'll query the add button and give it an event listener function that 
@@ -28,6 +28,9 @@ An exercise in putting todo lists on the DOM.
 ### Stretch Goals
 
 * Add some flexbox or grid so that our list looks good. Consider the type of spacing you want for your items, your input boxes, your title, and so on.
+  
 * Number the todo items (you don't have to use JavaScript for this, though you could) and ask the user for the _number_ they want to remove, not the index. You'll have to translate the user's input to remove the correct element from the array--humans and robots count differently!
+
 * Make it so that we can click on a todo list item to remove it instead of asking your user to type in a number. That's just awkward!
+
 * Add a button to delete all items. ("Clear List", maybe?). Make sure you're removing them from the DOM _and_ the array. You already have a function to remove all items from the DOM!
--- main.js
+++ main.js
@@ -1,37 +1,51 @@
 const todoList = []
 
-function printToDo(todo){
-    let newli = document.createElement(li)
-    let ul = document.querySelector('.todoList')
-
-    newli.innerText = todo
+function printToDo(text){
+    let ul = document.querySelector('.todo-list')
+    let li = document.createElement('li')
+    
+    li.innerText = text;
     ul.appendChild(li)
 }
-
-function addSomething(){
-    let text = document.querySelector('.todo-input')
-    let textAdded = text
-    
-    addOne(textAdded)
-    printToDo(textAdded)
-}
-
-
-//const addButton = document.getElementsByClassName('.add-todo')
-const addButton = document.querySelector('.add-todo')
-addButton.addEventListener('click', addSomething)
 
 function addOne(todo){
     todoList.push(todo)
 }
 
+function addSomething(){
+    let text = document.querySelector('.todo-input');
+    let textAdded = text.value;
+    
+    addOne(textAdded)
+    printToDo(textAdded)
+}
+
+//const addButton = document.getElementsByClassName('.add-todo')
+const addButton = document.querySelector('.add-todo')
+addButton.addEventListener('click', addSomething)
+
+function clearAll(){
+    location.reload()
+}
+
+const clearButton = document.querySelector('.remove-all')
+clearButton.addEventListener('click', clearAll)
+
+function removeSomething(input){
+    let text = document.querySelector('.index-input')
+    let textRemoved = text.value
+    
+    removeOne(textRemoved, input)
+    printToDo(textRemoved)
+}
+
+const removeButton = document.querySelector('.remove-todo')
+removeButton.addEventListener('click', removeOne)
+
 function removeOne(i){
     todoList.splice(i,0)
 }
 
-function clearAll(){
-    todoList.splice(todoList)
-}
 
 function printIndy(){
     for(const todo of todoList){
